9d6ca412583939dbff77ecb1b6abff2cd218bbce
csabater <csabater@hitpart.fr>
2018-03-08 10:38:50 +0100

    reset dashboard before new implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,6 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/takeWhile';
 import 'rxjs/add/operator/startWith';
 
-class TileSize {
-  id: string;
-  cols: number;
-  rows: number;
-  color: string;
-
-  constructor(id: string, cols: number, rows: number) {
-    this.id = id;
-    this.cols = cols;
-    this.rows = rows;
-  }
-
-}
 
 @Component({
   selector: 'app-root',
@@ -27,18 +14,7 @@ class TileSize {
 })
 export class AppComponent implements OnInit {
 
-  @Input()
-  public cols = 18;
-  @Input()
-  public menu: TileSize;
-  @Input()
-  public side: TileSize;
-  @Input()
-  public main: TileSize;
-  @Input()
-  public foot: TileSize;
-
-  constructor(private observableMedia: ObservableMedia) {}
+  constructor() {}
 
   ngOnInit() {
     const grid = new Map([
@@ -49,69 +25,7 @@ export class AppComponent implements OnInit {
       ['xl', 24]
     ]);
 
-    // deffinition of the grid occupation
-    // for each tile and each media size.
-    const gridOccupation = new Map<string, TileSize[]>([
-      ['xs', [
-        new TileSize('menu-bar', 8, 1),
-        new TileSize('side-nav', 0, 0),
-        new TileSize('main-content', 8, 8),
-        new TileSize('footer', 0, 0)
-      ]],
-      ['sm', [
-        new TileSize('menu-bar', 12, 1),
-        new TileSize('side-nav', 2, 10),
-        new TileSize('main-content', 10, 10),
-        new TileSize('footer', 0, 0)
-      ]],
-      ['md', [
-        new TileSize('menu-bar', 12, 1),
-        new TileSize('side-nav', 2, 10),
-        new TileSize('main-content', 10, 8),
-        new TileSize('footer', 12, 2)
-      ]],
-      ['lg', [
-        new TileSize('menu-bar', 18, 2),
-        new TileSize('side-nav', 3, 12),
-        new TileSize('main-content', 15, 10),
-        new TileSize('footer', 18, 2)
-      ]],
-      ['xl', [
-        new TileSize('menu-bar', 24, 2),
-        new TileSize('side-nav', 4, 10),
-        new TileSize('main-content', 20, 10),
-        new TileSize('footer', 24, 2)
-      ]],
-
-    ]);
-
-    this.observableMedia.subscribe(mediaChange => {
-      this.cols = grid.get(mediaChange.mqAlias);
-      this.assignTileSizes(mediaChange.mqAlias, gridOccupation);
-    });
   }
 
-
-  assignTileSizes(mediaAlias: string,
-                  mapTiles: Map<string, TileSize[]> ): void {
-    mapTiles.get(mediaAlias).forEach(tile => {
-      switch (tile.id) {
-        case 'menu-bar' :
-          this.menu = tile;
-          break;
-        case 'side-nav' :
-          this.side = tile;
-          break;
-        case 'main-content' :
-          this.main = tile;
-          break;
-        case 'footer' :
-          this.foot = tile;
-          break;
-        default:
-          console.log(`this tile name is not recognized..., tile considered = ${tile}`);
-      }
-    });
-  }
 
 }
